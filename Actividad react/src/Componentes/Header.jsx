6fefafbae3361538037f7f0a130d6ce6fc590094
LauraBenavidez <129073960+LauraBenavidez@users.noreply.github.com>
2023-06-08 10:35:10 -0500

    Actividad
    Paq}

---
+++ Actividad react/src/Componentes/Header.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+function Header() {
+    return (
+    <header>
+        <div>
+        <h1>Header</h1>
+            </div>
+                <nav>
+                    <ul>
+                        <li><a href="#">Laura</a></li>
+                        <li><a href="#">estefania</a></li>
+                        <li><a href="#">Vanegas</a></li>
+                    </ul>
+                </nav>
+    </header>
+    );
+}
+
+export default Header;
---
+++ Actividad react/src/Componentes/Main.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Main() {
+    return (
+    <main>
+        <h1>Main</h1>
+        <p>Laura es la mas linda</p>
+        
+    </main>
+    );
+}
+
+export default Main;
